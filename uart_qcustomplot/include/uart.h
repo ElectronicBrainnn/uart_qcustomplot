19627d0be55bfaf1c1af5ad616f631b5a604c556
ElectronicBrainnn <73044505+ElectronicBrainnn@users.noreply.github.com>
2023-04-01 22:05:54 +0700

    First commit

---
+++ uart_qcustomplot/include/uart.h
@@ -0,0 +1,37 @@
+#ifndef UART_H
+#define UART_H
+
+#include <QThread>
+#include <QObject>
+#include <QSerialPort>
+#include <QByteArray>
+#include <QTime>
+
+class UART : public QThread
+{
+    Q_OBJECT
+public:
+    explicit UART(QObject *parent = nullptr);
+    bool connect(QString nameCOM, quint32 baud);
+    void disconnect();
+    void transmit(QList<QString> transData);
+    bool stateConnect();
+
+signals:
+    void response(QList<QByteArray>);
+    void errorConnect(QString);
+    void timeout(QString);
+
+private slots:
+    void receive();
+
+protected:
+    void run();
+
+private:
+    QSerialPort serial;
+    bool COMConnect = false;
+    QList<QByteArray> resMessBuff;
+};
+
+#endif // UART_H
